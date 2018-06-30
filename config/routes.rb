346b69117e121786021fb540ea71ca82f7457a1d
Bianca Hale <sashabtt@gmail.com>
2018-06-30 10:20:12 -0600

    created controllers
    Reviewers: #the_pie_hole, awinitzky
    
    Reviewed By: #the_pie_hole, awinitzky
    
    Differential Revision: http://phabricator.devpointstudios.com/D2264

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'carts/index'
+
+  get 'carts/show'
+
+  get 'carts/create'
+
+  get 'carts/destroy'
+
+  get 'items/index'
+
+  get 'items/show'
+
+  get 'items/create'
+
+  get 'items/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     #API ROUTES SHOULD GO HERE
