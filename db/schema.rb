0251036a4997be313fc9a3e76a6379ee438b93d1
Jared Menard <jared.d.menard@gmail.com>
2018-06-30 10:54:20 -0600

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180630160219) do
+ActiveRecord::Schema.define(version: 20180630161337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "item_count"
+    t.float "cart_total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.float "price"
@@ -54,4 +72,5 @@ ActiveRecord::Schema.define(version: 20180630160219) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "carts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+counter = 1
+10.times do
+  name = Faker::Name.name
+  email = "test#{counter}@test.com"
+  password = "password"
+  password_confirmation = "password"
+  role = ['admin', 'guest'].sample
+  User.create(name: name, email: email, password: password, password_confirmation: password_confirmation, role: role)
+  puts "Created user: #{name}, #{email}, #{role}"
+  counter += 1
+end
+
+30.times do
+  name = Faker::Food.dish
+  description = Faker::FamilyGuy.quote + ' ' + Faker::FamilyGuy.quote
+  price = rand(30) + 6.95
+  quantity = rand(50) + 5
+  Item.create(name: name, description: description, price: price, quantity: quantity)
+  puts "Created item: #{name}, #{description}, $#{price}, quantity: #{quantity}"
+end
+
