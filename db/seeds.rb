4b54c282e87d6e2d0b61eeb523787ca88e32c999
Jared Menard <jared.d.menard@gmail.com>
2018-06-30 10:54:20 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-10.times do
-  name = Faker::Food.dish
-  description = Faker::FamilyGuy.quote + ' ' + Faker::FamilyGuy.quote
-  price = 10 + rand(30)
-  quantity = 5 + rand(100)
-  Item.create(name: name, description: description, price: price, quantity: quantity)
-  puts "Created food item: #{name}, description: #{description}, price: $#{price}, quantity: #{quantity}"
-end
-
-=======
->>>>>>> mybranch1
 counter = 1
 10.times do
   name = Faker::Name.name
@@ -18,12 +6,6 @@ counter = 1
   password_confirmation = "password"
   role = ['admin', 'guest'].sample
   User.create(name: name, email: email, password: password, password_confirmation: password_confirmation, role: role)
-<<<<<<< HEAD
-  puts "Created user: #{name}, email: #{email}, role: #{role}"
-  counter += 1
-end
-
-=======
   puts "Created user: #{name}, #{email}, #{role}"
   counter += 1
 end
@@ -36,5 +18,4 @@ end
   Item.create(name: name, description: description, price: price, quantity: quantity)
   puts "Created item: #{name}, #{description}, $#{price}, quantity: #{quantity}"
 end
->>>>>>> mybranch1
 
