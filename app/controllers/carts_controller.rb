df1e0b17c23e708df6fc76145fcb3ae994054af9
Bianca Hale <sashabtt@gmail.com>
2018-06-30 10:38:10 -0600

    completed carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,43 @@
 class CartsController < ApplicationController
-  def index
-  end
+  before_action :set_cart, only: [:show, :update, :destroy]
 
-  def show
-  end
+    def index
+        cars = Cart.all
+        render json: cart 
+    end
 
-  def create
-  end
+    def show
+        render json: @cart
+    end
 
-  def destroy
-  end
+    def create
+        cart = Cart.new(cart_params)
+        if cart.save
+            render json: cart
+        else
+            render json: {errors: cart.errors.full_messages}, status: 422
+        end
+    end
+
+    def update
+        if @cart.update(cart_params)
+            render json: @cart
+        else
+            render json: {errors: @cart.errors.full_messages}, status: 422
+         end
+    end
+
+    def destroy
+        @cart.destroy 
+    end
+
+    private
+
+    def set_cart
+        @cart = Cart.find(params[:id])
+    end
+
+    def cart_params
+        params.require(:cart).permit(:item_count, :cart_total)
+    end
 end
