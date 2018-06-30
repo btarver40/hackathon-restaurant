2579e1061aae163d05027b98b03170ad7a8002dd
Jared Menard <jared.d.menard@gmail.com>
2018-06-30 11:05:01 -0600

    dunno

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,7 @@
 Rails.application.routes.draw do
-  get 'carts/index'
-
-  get 'carts/show'
-
-  get 'carts/create'
-
-  get 'carts/destroy'
-
-  get 'items/index'
-
-  get 'items/show'
-
-  get 'items/create'
-
-  get 'items/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources :items
   end
 
   #Do not place any routes below this one
