9b724b2e8e451d7aec8b3cb10147b34b799bd7aa
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-06-30 11:06:11 -0600

    wip items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+  
   def index
+    render json: Item.all
   end
 
   def show
+    render json: @item
   end
 
   def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: item
+    else
+      render json: item.errors, status: 422
+    end
   end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: 422
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def product_params
+    params.require(:item).permit(:)
 
   def destroy
   end
